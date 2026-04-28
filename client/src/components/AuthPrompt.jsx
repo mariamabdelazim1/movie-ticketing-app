f3d2ef26132f4cec85862b5fcd8d72e17ea88cd4
MalakSabraa <malaksabraa18@gmail.com>
2026-04-28 20:48:04 +0300

    updates

---
+++ client/src/components/AuthPrompt.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./AuthPrompt.css";
+
+export function AuthPrompt() {
+  return <section className="panel auth-prompt">Please login or sign up first.</section>;
+}
